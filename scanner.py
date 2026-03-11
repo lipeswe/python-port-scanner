424b66c5698fd96f09b458f6cc06eebd61c69bfa
Fellipe Dutra <a60832@alunos.ipb.pt>
2026-03-11 12:18:59 +0000

    Add initial TCP port scanner script

---
+++ scanner.py
@@ -0,0 +1,20 @@
+import socket
+
+target = input("Enter target IP or hostname: ")
+
+ports = [21,22,23,25,53,80,110,139,143,443]
+
+print(f"\nScanning {target}\n")
+
+for port in ports:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print(f"[OPEN] Port {port}")
+    else:
+        print(f"[CLOSED] Port {port}")
+
+    s.close()
